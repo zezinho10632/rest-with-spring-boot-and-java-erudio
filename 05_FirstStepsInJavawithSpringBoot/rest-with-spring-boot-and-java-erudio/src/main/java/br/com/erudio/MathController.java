d9e14acb36090c57b1c9c55b006680bdf520f606
José <jn10632@gmail.com>
2024-11-19 04:49:44 -0300

    implementaçao de Exceptions e operaçao matematica de soma

---
+++ 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/MathController.java
@@ -0,0 +1,36 @@
+package br.com.erudio;
+
+import br.com.erudio.exceptions.UnsupportedMathOperationException;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MathController {
+
+    @RequestMapping(value="/sum/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+    public Double sum(@PathVariable("numberOne") String numberOne,
+                      @PathVariable("numberTwo") String numberTwo) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Please set a Numeric Value");
+        }
+        return covertToDouble(numberOne) + covertToDouble(numberTwo);
+    }
+
+    private  Double covertToDouble(String strNumber)
+    {
+        if(strNumber == null) return 0D;
+        // BR 10,25US 10,25
+        String number = strNumber.replaceAll(",", ".");
+        if (isNumeric(number)) return Double.parseDouble(number);
+        return 0D;
+    }
+
+    private boolean isNumeric(String strNumber)
+    {
+        if(strNumber == null) return false;
+        String number = strNumber.replaceAll(",", ".");
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+}
